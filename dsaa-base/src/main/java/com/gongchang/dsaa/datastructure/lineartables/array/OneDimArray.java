4df15f07aa5df46160ae22df386d7f76dfb8eb5a
zjf-gongchang <zjf_gongchang@163.com>
2023-08-19 23:48:41 +0800

    基础算法初始化

--- dsaa-base/src/main/java/com/gongchang/dsaa/datastructure/lineartables/array/OneDimArray.java
+++ dsaa-base/src/main/java/com/gongchang/dsaa/datastructure/lineartables/array/OneDimArray.java
@@ -1,5 +1,7 @@
 package com.gongchang.dsaa.datastructure.lineartables.array;
 
+import java.util.Arrays;
+
 /**
  * 一维数组
  * 
@@ -8,4 +10,16 @@ package com.gongchang.dsaa.datastructure.lineartables.array;
  */
 public class OneDimArray {
 
+	public static void main(String[] args) {
+		
+		int[] tmpIntArr = {1,2,3,4,5,6,7,8,9,10};
+		
+		int[] intArr = new int[tmpIntArr.length];
+		
+		for(int i=0; i<tmpIntArr.length; i++){
+			intArr[i] = tmpIntArr[i];
+		}
+		
+		System.out.println(Arrays.toString(intArr));
+	}
 }
--- dsaa-base/src/main/java/com/gongchang/dsaa/datastructure/lineartables/array/TwoDimArray.java
+++ dsaa-base/src/main/java/com/gongchang/dsaa/datastructure/lineartables/array/TwoDimArray.java
@@ -1,5 +1,9 @@
 package com.gongchang.dsaa.datastructure.lineartables.array;
 
+import java.util.Arrays;
+
+import javax.xml.transform.Templates;
+
 /**
  * 二维数组
  * 
@@ -8,4 +12,34 @@ package com.gongchang.dsaa.datastructure.lineartables.array;
  */
 public class TwoDimArray {
 
+	public static void main(String[] args) {
+		
+		int[][] tmpIntArr = {
+				{1,2,3,4,5,6,7,8,9,10},
+				{1,2,3,4,5,6,7},
+				{1,2,3,4,5,6,7,8,9},
+				{1,2,3},
+				{1,2,3,4},
+				{1,2,3,4,5,6,7,8,9,10},
+				{1},
+				{1,2,3,4,5},
+				{1,2,3,4,5,6},
+				{1,2}
+		};
+		
+		int[][] intArr = new int[tmpIntArr.length][];
+		
+		for(int i=0; i<tmpIntArr.length; i++){
+			int[] eleArr = new int[tmpIntArr[i].length];
+			for(int j=0; j<tmpIntArr[i].length; j++){
+				eleArr[j] = tmpIntArr[i][j];
+			}
+			intArr[i] = eleArr;
+		}
+		
+		for(int i=0; i<intArr.length; i++){
+			System.out.println(Arrays.toString(intArr[i]));
+		}
+		
+	}
 }
