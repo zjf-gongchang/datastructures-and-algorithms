ac3fead9eee62bf55d919eba2f4cb6cf6bba0ed9
zjf-gongchang <zjf_gongchang@163.com>
2023-09-02 23:26:07 +0800

    基础算法初始化

--- dsaa-base/src/main/java/com/gongchang/dsaa/algorithm/sort/BucketSort.java
+++ dsaa-base/src/main/java/com/gongchang/dsaa/algorithm/sort/BucketSort.java
@@ -1,5 +1,9 @@
 package com.gongchang.dsaa.algorithm.sort;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 桶排序
  * 
@@ -13,5 +17,64 @@ package com.gongchang.dsaa.algorithm.sort;
  *
  */
 public class BucketSort {
+	
+    public static void bucketSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
 
+        int min = arr[0];
+        int max = arr[0];
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        int bucketCount = (max - min) / arr.length + 1;
+        List<List<Integer>> buckets = new ArrayList<>(bucketCount);
+
+        for (int i = 0; i < bucketCount; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for (int num : arr) {
+            int index = (num - min) / arr.length;
+            buckets.get(index).add(num);
+        }
+
+        for (List<Integer> bucket : buckets) {
+            Collections.sort(bucket);
+        }
+
+        int index = 0;
+        for (List<Integer> bucket : buckets) {
+            for (int num : bucket) {
+                arr[index++] = num;
+            }
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("原始数组:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        bucketSort(arr);
+
+        System.out.println("排序后的数组:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
+
--- dsaa-base/src/main/java/com/gongchang/dsaa/algorithm/sort/CountingSort.java
+++ dsaa-base/src/main/java/com/gongchang/dsaa/algorithm/sort/CountingSort.java
@@ -7,11 +7,59 @@ package com.gongchang.dsaa.algorithm.sort;
  * 
  * 核心思想：
  * 
- * 时间复杂度：
+ * 时间复杂度：O(n + k)
  * 
  * @author GongChang
  *
  */
 public class CountingSort {
 
+    public static void countingSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        // 1. 找到数组中的最大值
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        // 2. 创建计数数组并初始化
+        int[] countArray = new int[max + 1];
+        for (int num : arr) {
+            countArray[num]++;
+        }
+
+        // 3. 根据计数数组重新构建排序后的数组
+        int index = 0;
+        for (int i = 0; i <= max; i++) {
+            while (countArray[i] > 0) {
+                arr[index++] = i;
+                countArray[i]--;
+            }
+        }
+    }
+
+    // 测试计数排序
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 2, 8, 3, 3, 1};
+
+        System.out.println("原始数组:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        countingSort(arr);
+
+        System.out.println("排序后的数组:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
+
